36bf00edf5c0561815e84cbf9484b2124f8fe63b
Dennis Rasmussen <denn5682@gmail.com>
2023-11-02 15:53:20 +0100

    Added Testsuite to Solution

---
+++ Web_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Web_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
